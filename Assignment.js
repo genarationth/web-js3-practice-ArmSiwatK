a66f376b93b7377c1b9099085b9f3d1a879ceaf0
Siwat Kanjanapanitkun <125792234+ArmSiwatK@users.noreply.github.com>
2023-03-13 16:18:29 +0700

    Assignment Turn In

---
+++ Assignment.js
@@ -0,0 +1,16 @@
+add = (a, b) => console.log(a + b);
+subtract = (a, b) => console.log(a - b);
+multiply = (a, b) => console.log(a * b);
+divide = (a, b) => console.log(a / b);
+
+inform = (firstName, location, hobby) => console.log(`Hi, my name is ${firstName}. I live in ${location} and enjoy ${hobby}.`);
+
+
+
+//Test
+
+add(1, 2);
+subtract(4, 2);
+multiply(5, 1);
+divide(6, 3);
+inform('Arm', 'Si Racha', 'animating');
